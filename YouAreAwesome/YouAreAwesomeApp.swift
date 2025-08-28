178d5502c16aa2122b22284c17f111d01da03adb
Min-Seok Pang <mins.pang@gmail.com>
2025-08-28 12:49:46 -0500

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Min-Seok Pang on 8/28/25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
